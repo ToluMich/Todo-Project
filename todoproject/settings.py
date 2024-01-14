43ce184101509f63672dcc4a7c38a77e66b5447f
Mic-bot-sys <michaelstolu@gmail.com>
2024-01-14 18:15:39 +0100

    changed debug to True to make some confirmations

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70uequ7-#c3q#$cd_!$f*a@t&z0eo%7-f_9t04gamakp)9!^qm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
